9f5a69fcb0b2f370ae1a9917a49a734716bc4fe0
Ben Pollack <benpollack8723@gmail.com>
2024-06-24 17:37:37 -0500

    updated files and added css file

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,16 +1,42 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+const maxItems = 5;
+const addEraserBtn = document.getElementById('add-eraser-to-cart');
+const addNotebookBtn = document.getElementById('add-notebook-to-cart');
+const addPencilBtn = document.getElementById('add-pencil-to-cart');
+const addRulerBtn = document.getElementById('add-ruler-to-cart');
+const cartItems = document.getElementById('cart-items');
+const clearCartBtn = document.getElementById('clear-cart');
+let itemCount = 0;
 
+function addItem(item) {
+    basket.push(item);
+    console.log(basket);
+    return true;
+};
 
+function listItem(item){
+for (item of basket) {
 
+}
+}
+function removeItem() {
 
+};
+function empty() {
+    basket = [];
+    console.log(basket);
+}
 
+addEraserBtn.addEventListener('click', ()=> {
+    addItem('Eraser');
+})
 
-
-
-
-
+clearCartBtn.addEventListener('click', ()=>{
+empty();
+})
 
 // DO NOT MODIFY
 // Used for automated testing
